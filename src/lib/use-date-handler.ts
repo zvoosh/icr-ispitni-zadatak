fa0e2146d28747f2f4ff3155e14072abb9afc8eb
dusan.ilic <dusan.ilic@factoryww.com>
2023-02-02 19:11:05 +0100

    fix: fix

--- src/lib/use-date-handler.ts
+++ src/lib/use-date-handler.ts
@@ -1,27 +1,33 @@
-import { format, parseISO } from 'date-fns';
-import moment from 'moment';
-import { DATE_FORMAT } from '../types';
+// import { format } from 'date-fns';
+import { format, parseISO } from "date-fns";
+import moment from "moment";
+import { DATE_FORMAT } from "../types";
 
-const useDate = () => {
-    const dateFormat = (date: Date | string, dateFormat = DATE_FORMAT.ddMMyyyy) => {
-        return format(new Date(date), dateFormat);
-    };
+export const useDate = () => {
+  const dateFormat = (
+    date: Date | string,
+    dateFormat = DATE_FORMAT.ddMMyyyy
+  ) => {
+    return format(new Date(date), dateFormat); //undefiend
+  };
 
-    const parseIso = (date: string) => parseISO(date);
+  const parseIso = (date: string) => parseISO(date);
 
-    const parseToMoment = (date: string, dateFormat = DATE_FORMAT.ddMMyyyy) => moment(date, dateFormat);
+  const parseToMoment = (date: string, dateFormat = DATE_FORMAT.ddMMyyyy) =>
+    moment(date, dateFormat);
 
-    const dateFormatFromIso = (date: string, dateFormat = DATE_FORMAT.ddMMyyyy) => {
-        const parsedDate = parseIso(date);
-        return format(new Date(parsedDate), dateFormat);
-    };
+  const dateFormatFromIso = (
+    date: string,
+    dateFormat = DATE_FORMAT.ddMMyyyy
+  ) => {
+    const parsedDate = parseIso(date);
+    return format(new Date(parsedDate), dateFormat);
+  };
 
-    return {
-        dateFormat,
-        parseIso,
-        parseToMoment,
-        dateFormatFromIso,
-    };
+  return {
+    dateFormat,
+    parseIso,
+    parseToMoment,
+    dateFormatFromIso,
+  };
 };
-
-export { useDate };
